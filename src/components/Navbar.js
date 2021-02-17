ff01c6cb109471ab2d75661e20d19159dbc5647f
severovicivan <ivan.severovic@student.uniri.hr>
2021-02-17 16:26:47 +0100

    Navbar Component and CSS updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,25 @@ import logo from '../logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar component</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="zoyya logo" className="logo"></img>
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">
+              Početna
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">
+              Dokumentacija
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
